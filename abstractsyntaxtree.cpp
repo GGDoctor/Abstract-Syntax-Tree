6faa68a0ec6cc887fefdbfc87a0b225973363957
Hbjoerger <97217742+Hbjoerger@users.noreply.github.com>
2024-04-17 12:31:04 -0700

    Semicolon work
    Semicolon work

--- abstractsyntaxtree.cpp
+++ abstractsyntaxtree.cpp
@@ -8,7 +8,8 @@
  * @param token - A token object
  * @returns The actual character(s) of the token
  */
-inline auto toTokenCharacter(Token token) {
+inline auto toTokenCharacter(Token token)
+{
     return token.character;
 }
 
@@ -17,116 +18,117 @@ inline auto toTokenCharacter(Token token) {
  * @param token - A Token object
  * @returns A string of the token type instead of 0, 1, 2, ...
  */
-inline auto toTokenType(Token token) {
-    switch (token.type) {
-        case CHAR:
-            return "CHAR";
+inline auto toTokenType(Token token)
+{
+    switch (token.type)
+    {
+    case CHAR:
+        return "CHAR";
 
-        case VOID:
-            return "VOID";
+    case VOID:
+        return "VOID";
 
-        case IDENTIFIER:
-            return "IDENTIFIER";
+    case IDENTIFIER:
+        return "IDENTIFIER";
 
-        case INTEGER:
-            return "INTEGER";
+    case INTEGER:
+        return "INTEGER";
 
-        case STRING:
-            return "STRING";
+    case STRING:
+        return "STRING";
 
-        case LEFT_PARENTHESIS:
-            return "LEFT_PARENTHESIS";
+    case LEFT_PARENTHESIS:
+        return "LEFT_PARENTHESIS";
 
-        case RIGHT_PARENTHESIS:
-            return "RIGHT_PARENTHESIS";
+    case RIGHT_PARENTHESIS:
+        return "RIGHT_PARENTHESIS";
 
-        case LEFT_BRACKET:
-            return "LEFT_BRACKET";
+    case LEFT_BRACKET:
+        return "LEFT_BRACKET";
 
-        case RIGHT_BRACKET:
-            return "RIGHT_BRACKET";
+    case RIGHT_BRACKET:
+        return "RIGHT_BRACKET";
 
-        case LEFT_BRACE:
-            return "LEFT_BRACE";
+    case LEFT_BRACE:
+        return "LEFT_BRACE";
 
-        case RIGHT_BRACE:
-            return "RIGHT_BRACE";
+    case RIGHT_BRACE:
+        return "RIGHT_BRACE";
 
-        case DOUBLE_QUOTE:
-            return "DOUBLE_QUOTE";
+    case DOUBLE_QUOTE:
+        return "DOUBLE_QUOTE";
 
-        case SINGLE_QUOTE:
-            return "SINGLE_QUOTE";
+    case SINGLE_QUOTE:
+        return "SINGLE_QUOTE";
 
-        case SEMICOLON:
-            return "SEMICOLON";
+    case SEMICOLON:
+        return "SEMICOLON";
 
-        case COMMA:
-            return "COMMA";
+    case COMMA:
+        return "COMMA";
 
-        case ASSIGNMENT:
-            return "ASSIGNMENT";
+    case ASSIGNMENT:
+        return "ASSIGNMENT";
 
-        case PLUS:
-            return "PLUS";
+    case PLUS:
+        return "PLUS";
 
-        case MINUS:
-            return "MINUS";
+    case MINUS:
+        return "MINUS";
 
-        case DIVIDE:
-            return "DIVIDE";
+    case DIVIDE:
+        return "DIVIDE";
 
-        case ASTERISK:
-            return "ASTERISK";
+    case ASTERISK:
+        return "ASTERISK";
 
-        case MODULO:
-            return "MODULO";
+    case MODULO:
+        return "MODULO";
 
-        case CARAT:
-            return "CARAT";
+    case CARAT:
+        return "CARAT";
 
-        case LT:
-            return "LT";
+    case LT:
+        return "LT";
 
-        case GT:
-            return "GT";
+    case GT:
+        return "GT";
 
-        case LT_EQUAL:
-            return "LT_EQUAL";
+    case LT_EQUAL:
+        return "LT_EQUAL";
 
-        case GT_EQUAL:
-            return "GT_EQUAL";
-    
-        case BOOLEAN_AND_OPERATOR:
-            return "BOOLEAN_AND_OPERATOR";
+    case GT_EQUAL:
+        return "GT_EQUAL";
 
-        case BOOLEAN_OR_OPERATOR:
-            return "BOOLEAN_OR_OPERATOR";
+    case BOOLEAN_AND_OPERATOR:
+        return "BOOLEAN_AND_OPERATOR";
 
-        case BOOLEAN_NOT_OPERATOR:
-            return "BOOLEAN_NOT_OPERATOR";
+    case BOOLEAN_OR_OPERATOR:
+        return "BOOLEAN_OR_OPERATOR";
 
-        case BOOLEAN_EQUAL:
-            return "BOOLEAN_EQUAL";
+    case BOOLEAN_NOT_OPERATOR:
+        return "BOOLEAN_NOT_OPERATOR";
 
-        case BOOLEAN_NOT_EQUAL:
-            return "BOOLEAN_NOT_EQUAL";
+    case BOOLEAN_EQUAL:
+        return "BOOLEAN_EQUAL";
 
-        case BOOLEAN_TRUE:
-            return "BOOLEAN_TRUE";
+    case BOOLEAN_NOT_EQUAL:
+        return "BOOLEAN_NOT_EQUAL";
 
-        case BOOLEAN_FALSE:
-            return "BOOLEAN_FALSE";
+    case BOOLEAN_TRUE:
+        return "BOOLEAN_TRUE";
 
+    case BOOLEAN_FALSE:
+        return "BOOLEAN_FALSE";
     }
 }
 
 /**
  * @brief Constructor
- * @param tokens - A vector of tokens from a C-style program 
+ * @param tokens - A vector of tokens from a C-style program
  */
-AbstractSyntaxTree::AbstractSyntaxTree(const vector<Token>& tokens) {
-    
+AbstractSyntaxTree::AbstractSyntaxTree(const vector<Token> &tokens)
+{
 }
 
 /**
@@ -134,37 +136,42 @@ AbstractSyntaxTree::AbstractSyntaxTree(const vector<Token>& tokens) {
  * @param concreteSyntaxTree - The concrete syntax tree that was generated in
  *                                                      RecursiveDescentParser
  */
-AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree) {
-    LCRS* cst = concreteSyntaxTree.getConcreteSyntaxTree();
+AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree)
+{
+    LCRS *cst = concreteSyntaxTree.getConcreteSyntaxTree();
 
-    //cout << cst->token.character << "^^^^^\n";
+    // cout << cst->token.character << "^^^^^\n";
 
-    vector<vector<Token>> result;  // 2D vector to hold the tokens by levels
-    queue<pair<LCRS*, int>> q;     // Queue to hold nodes along with their level
+    vector<vector<Token>> result; // 2D vector to hold the tokens by levels
+    queue<pair<LCRS *, int>> q;   // Queue to hold nodes along with their level
     q.push({cst, 0});             // Start with the root at level 0
 
-    while (!q.empty()) {
-        auto front = q.front();  // Get the front item (node and its level)
+    while (!q.empty())
+    {
+        auto front = q.front(); // Get the front item (node and its level)
         q.pop();
 
-        LCRS* currentNode = front.first;
+        LCRS *currentNode = front.first;
         int level = front.second;
 
         // Ensure the vector is large enough to hold this level
-        if (level >= result.size()) {
-            result.resize(level + 1);  // Resize to accommodate new level
+        if (level >= result.size())
+        {
+            result.resize(level + 1); // Resize to accommodate new level
         }
 
         // Add the current node's token to the appropriate level
         result[level].push_back(currentNode->token);
 
         // Enqueue left child if exists, at the next level
-        if (currentNode->leftChild) {
+        if (currentNode->leftChild)
+        {
             q.push({currentNode->leftChild, level + 1});
         }
 
         // Enqueue right sibling if exists, at the same level
-        if (currentNode->rightSibling) {
+        if (currentNode->rightSibling)
+        {
             q.push({currentNode->rightSibling, level});
         }
     }
@@ -179,8 +186,8 @@ AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree
     }
     */
 
-    LCRS* ast = nullptr;//new LCRS(result[0]);
-    LCRS* temp = ast;
+    LCRS *ast = nullptr; // new LCRS(result[0]);
+    LCRS *temp = ast;
 
     /*
     for (int i = 0; i < result.size(); i++) {
@@ -207,18 +214,21 @@ AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree
         }
 
         for (int j = 0; j < result[i].size(); j++) {
-            
+
         }
     }
     */
 
     vector<vector<Token>> abstract;
 
-    for (int i = 0; i < result.size(); i++) {
+    for (int i = 0; i < result.size(); i++)
+    {
         vector<Token> k;
-        for (int j = 0; j < result[i].size(); j++) {
+        for (int j = 0; j < result[i].size(); j++)
+        {
             Token token;
-            if (isDeclarationKeyword(result[i][0].character)) {
+            if (isDeclarationKeyword(result[i][0].character))
+            {
                 token.character = "Declaration";
                 token.type = result[i][0].type;
                 token.lineNumber = result[i][0].lineNumber;
@@ -226,29 +236,64 @@ AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree
                 break;
             }
 
-            if (result[i][0].character == "{") {
+            //if statement
+            if (isDeclarationKeyword(result[i][0].character))
+            {
+                token.character = "If";
+                token.type = result[i][0].type;
+                token.lineNumber = result[i][0].lineNumber;
+                k.push_back(token);
+                break;
+            }
+
+            if (result[i][0].character == "{")
+            {
                 result[i][0].character = "Begin Block";
                 k.push_back(result[i][0]);
                 break;
             }
 
-            if (result[i][0].character == "}") {
+            if (result[i][0].character == "}")
+            {
                 result[i][0].character = "End Block";
                 k.push_back(result[i][0]);
                 break;
             }
 
-            if (result[i][1].character == "=") {
+            if (result[i][1].character == "=")
+            {
                 token.character = "Assignment";
                 token.type = result[i][0].type;
                 token.lineNumber = result[i][0].lineNumber;
                 k.push_back(token);
 
                 vector<Token> postfix = infixToPostfix(result[i]);
-                for (int r = 0; r < postfix.size(); r++) {
+                for (int r = 0; r < postfix.size(); r++)
+                {
                     k.push_back(postfix[r]);
                 }
                 break;
+            }
+
+            // new semicolon work
+            if (result[i][1].character == ";")
+            {
+                token.character = "Semicolon";
+                token.type = result[i][0].type;
+                token.lineNumber = result[i][0].lineNumber;
+                k.push_back(token);
+
+                vector<Token> postfix = infixToPostfix(result[i]);
+                for (int r = 0; r < postfix.size(); r++)
+                {
+                    k.push_back(postfix[r]);
+                }
+                break;
+            }
+
+            // print statement
+            if (result[i][1].character == "printf")
+            {
             }
 
             token.character = result[i][j].character;
@@ -259,8 +304,10 @@ AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree
         abstract.push_back(k);
     }
 
-    for (auto i: abstract) {
-        for (auto j : i) {
+    for (auto i : abstract)
+    {
+        for (auto j : i)
+        {
             cout << j.character << ' ';
         }
         cout << '\n';
@@ -276,8 +323,8 @@ AbstractSyntaxTree::AbstractSyntaxTree(RecursiveDescentParser concreteSyntaxTree
  *
  *      ex: cout << AbstractSyntaxTreeObj;
  */
-ostream &operator<<(ostream &os, const AbstractSyntaxTree &obj) {
+ostream &operator<<(ostream &os, const AbstractSyntaxTree &obj)
+{
     os << obj.abstractSyntaxTree->BFS();
     return os;
 }
-
